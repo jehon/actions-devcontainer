4545bf618bf5e1ce05a75784291ad9fe171dffaa
Jean Honlet <jehon@users.noreply.github.com>
2022-08-27 06:45:43 +0000

    test: add first tests

---
+++ tests/test-start-stop.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -o errexit
+
+# shellcheck source=/usr/bin/jh-lib
+. jh-lib
+
+R="$(dirname "$JH_SWD")/jh-github-dc"
+
+header_begin "start"
+$R start
+header_end
+
+header_begin "make dump"
+$R make dump
+header_end
+
+
+header_begin "stop"
+$R stop
+header_end
