6311a61cd2639db1c222ab13ca50043b2469a1bb
Jean Honlet <jehon@users.noreply.github.com>
2022-10-17 10:37:18 +0000

    fix: remove "make", dc-arg-*, dc-env-*

--- tests/test-start-stop.sh
+++ tests/test-start-stop.sh
@@ -17,29 +17,6 @@ header_begin "run make dc-hello (inline env)"
 $R run "CUSTOM_ENV=Human make dc-hello"
 header_end
 
-header_begin "make dump (legacy)"
-$R make dump
-header_end
-
-header_begin "run make dc-hello (DC_ENV_0 - legacy)"
-(
-    export DC_ENV_0="CUSTOM_ENV=Human"
-    $R dump
-    $R run "env"
-    $R run "make dc-hello"
-)
-header_end
-
-header_begin "run make dc-hello (DC_ARGS - legacy)"
-(
-    export DC_ARG_0="-e"
-    export DC_ARG_1="CUSTOM_ENV=Human"
-    $R dump
-    $R run "env"
-    $R run "make dc-hello"
-)
-header_end
-
 header_begin "run ls /"
 $R run "ls /"
 header_end
