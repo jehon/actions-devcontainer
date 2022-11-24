d905b972e0874bee7ca9ec92a85800d22ef96294
Jean Honlet <jehon@users.noreply.github.com>
2022-11-24 15:02:43 +0000

    style: remove ::group:: outside of CI

--- tests/lib.sh
+++ tests/lib.sh
@@ -3,13 +3,17 @@
 SWD="$(dirname "${BASH_SOURCE[0]}")"
 
 header_begin() {
+    if [ -n "$CI" ]; then
     echo "::group::{$1}"
+    fi
     echo "$1..."
 }
 
 header_end() {
     echo "...done"
+    if [ -n "$CI" ]; then
     echo "::endgroup::"
+    fi
 }
 
 R="$(dirname "$SWD")/jh-github-dc"
