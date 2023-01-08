639c0e4363eeccb43ad58308230bfcb45561fb02
Jean Honlet <jehon@users.noreply.github.com>
2023-01-08 08:54:52 +0000

    feat(js): enhance stub

---
+++ consts.js
@@ -0,0 +1,5 @@
+
+export const StateIsStarted = "isStarted";
+
+export const InputAction = "action";
+export const InputRun = "run";
--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { StateIsStarted, InputRun, InputAction } = require('./consts');
 
 try {
     //
@@ -10,18 +12,44 @@ try {
     // output:
     // - container (name)
     //
+    // state:
+    // - isStarted (boolean)
 
-    const nameToGreet = core.getInput('run');
-    console.log(`Hello ${nameToGreet}!`);
+    console.log(core.getSate(StateIsStarted))
 
-    core.setOutput("container", 'blablabla');
+    const action = core.getInput(InputAction);
 
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    switch(action) {
+        case 'start':
+            console.log("Starting devcontainer");
 
-    // core.saveState("pidToKill", 12345);
-    // var pid = core.getState("pidToKill");
+            // core.setOutput("container", 'blablabla');
+
+            core.setState(StateIsStarted, true);
+            break;
+
+            case 'stop':
+            console.log("Stopping devcontainer");
+            core.warn('Stopping is not implemented');
+            break;
+                
+        case '':
+            if (!core.getState(StateIsStarted)) {
+                core.error('Devcontainer was not started');
+            }
+            const run = core.getInput(InputRun);
+            console.log("Running");
+            break;
+    }
+
+
+    // const nameToGreet = core.getInput('run');
+    // console.log(`Hello ${nameToGreet}!`);
+
+
+    // // Get the JSON webhook payload for the event that triggered the workflow
+    // const payload = JSON.stringify(github.context.payload, undefined, 2)
+    // console.log(`The event payload: ${payload}`);
 
 } catch (error) {
     core.setFailed(error.message);
