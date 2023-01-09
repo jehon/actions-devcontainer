5dcce129a047a4c80185e18d26ad5067a247a747
Jean Honlet <jehon@users.noreply.github.com>
2023-01-09 12:34:45 +0000

    fix: dump command and take it from argv

--- main.js
+++ main.js
@@ -62,12 +62,17 @@ try {
             core.warning('Stopping is not implemented');
             break;
                 
+        case 'run':
         case '':
             if (!core.getState(StateIsStarted)) {
                 core.error('Devcontainer was not started');
+                console.log(core.getState(StateIsStarted));
             }
-            const run = core.getInput(InputRun);
-            console.log("Running");
+            const run = process.argv.length > 3
+                ? process.argv[3]
+                : core.getInput(InputRun)
+    
+            console.log(`Running ${run}`);
             break;
     }
 
