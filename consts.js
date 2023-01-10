46b58e6221cf1ceff8ce4ce6696cea4ab7e730b3
Jean Honlet <jehon@users.noreply.github.com>
2023-01-10 08:36:32 +0000

    fix: add swd

--- consts.js
+++ consts.js
@@ -3,6 +3,7 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
 export const actionPath = dirname(fileURLToPath(import.meta.url));
+export const swd = dirname(process.cwd());
 
 export const StateIsStarted = "isStarted";
 
